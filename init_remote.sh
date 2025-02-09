db0290aea89f36de2a534a58255425473571733e
Hanhong Zhao <zhh24@mit.edu>
2025-02-09 14:20:11 +0000

    remove dangerous

--- init_remote.sh
+++ init_remote.sh
@@ -3,20 +3,20 @@ source ka.sh # import VM_NAME, ZONE
 
 echo $VM_NAME $ZONE
 
-STAGEDIR=/$DATA_ROOT/staging/$(whoami)/debug/init
-sudo mkdir -p $STAGEDIR
-sudo chmod 777 $STAGEDIR
-echo 'Staging files...'
-sudo rsync -a requirements.txt $STAGEDIR
-sudo rsync -a 装牛牛X.sh $STAGEDIR
-echo 'staging dir: '$STAGEDIR
-echo 'Done staging.'
+# STAGEDIR=/$DATA_ROOT/staging/$(whoami)/debug/init
+# sudo mkdir -p $STAGEDIR
+# sudo chmod 777 $STAGEDIR
+# echo 'Staging files...'
+# sudo rsync -a requirements.txt $STAGEDIR
+# sudo rsync -a 装牛牛X.sh $STAGEDIR
+# echo 'staging dir: '$STAGEDIR
+# echo 'Done staging.'
 
-LOGDIR=$STAGEDIR/log
-sudo rm -rf $LOGDIR
-sudo mkdir -p ${LOGDIR}
-sudo chmod 777 ${LOGDIR}
-echo 'Log dir: '$LOGDIR
+# LOGDIR=$STAGEDIR/log
+# sudo rm -rf $LOGDIR
+# sudo mkdir -p ${LOGDIR}
+# sudo chmod 777 ${LOGDIR}
+# echo 'Log dir: '$LOGDIR
 
 
 # mount NFS Filestore
@@ -38,13 +38,13 @@ ls /kmh-nfs-ssd-eu-mount
 
 "
 
-gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE \
---worker=all --command "
-sudo rm -rf /home/\$(whoami)/.local
-cd $STAGEDIR
-echo 'Current dir: '
-pwd
-source 装牛牛X.sh
-"
+# gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE \
+# --worker=all --command "
+# sudo rm -rf /home/\$(whoami)/.local
+# cd $STAGEDIR
+# echo 'Current dir: '
+# pwd
+# source 装牛牛X.sh
+# "
 
 source setup_remote_wandb.sh
