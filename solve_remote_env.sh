9dadfce5bb9089d31b9d23be5855efcc0360026d
Hanhong Zhao <zhh24@mit.edu>
2025-02-27 15:15:08 +0000

    fix

--- solve_remote_env.sh
+++ solve_remote_env.sh
@@ -3,8 +3,11 @@ source ka.sh # import VM_NAME, ZONE
 echo 'solve'
 gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE --worker=all \
     --command "
+sudo lsof -w /dev/accel0 | grep main.py | grep -v 'grep' | awk '{print \"sudo kill -9 \" \$2}' | sh
 " # &> /dev/null
 echo 'solved!'
 # ls /home/sqa
 
 # pip3 install tensorstore==0.1.67
+
+# | grep main.py | grep -v 'grep' | awk '{print \"sudo kill -9 \" \$2}' | sh
