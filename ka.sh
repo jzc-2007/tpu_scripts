e18cf1825226eb9e3d1c316484ebe0cbdc75171f
Hanhong Zhao <zhh24@mit.edu>
2025-04-13 17:44:41 +0000

    added two cards

--- ka.sh
+++ ka.sh
@@ -27,7 +27,8 @@ fi
 # export VM_NAME=kmh-tpuvm-v2-32-6
 # export VM_NAME=kmh-tpuvm-v2-32-7
 # export VM_NAME=kmh-tpuvm-v2-32-8
-# export VM_NAME=kmh-tpuvm-v3-32-1
+# export VM_NAME=kmh-tpuvm-v4-8-1
+# export VM_NAME=kmh-tpuvm-v4-8-2
 # export VM_NAME=kmh-tpuvm-v4-8-6
 # export VM_NAME=kmh-tpuvm-v2-32-preemptible-1
 # export VM_NAME=kmh-tpuvm-v2-32-preemptible-2
@@ -35,7 +36,9 @@ fi
 # export VM_NAME=kmh-tpuvm-v4-32-preemptible-1
 # export VM_NAME=kmh-tpuvm-v4-32-preemptible-2
 # export VM_NAME=kmh-tpuvm-v4-32-preemptible-3
-export VM_NAME=kmh-tpuvm-v4-32-preemptible-4
+# export VM_NAME=kmh-tpuvm-v4-32-preemptible-4
+# export VM_NAME=kmh-tpuvm-v3-32-1
+# export VM_NAME=kmh-tpuvm-v3-32-5
 # export VM_NAME=kmh-tpuvm-v3-32-11
 # export VM_NAME=kmh-tpuvm-v3-32-12
 # export VM_NAME=kmh-tpuvm-v3-32-13
