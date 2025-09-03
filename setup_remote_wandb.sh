89786fef903cd97f7b20d5eb04325998f53dae70
qiaosungithub <sqa23@mails.tsinghua.edu.cn>
2025-09-03 20:59:56 +0000

    fix bug

--- setup_remote_wandb.sh
+++ setup_remote_wandb.sh
@@ -51,8 +51,8 @@ echo $VM_NAME $ZONE
 
 gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE \
     --worker=all --command "
-$CONDA_PY_PATH -m wandb login $WANDB_API_KEY
+python -m wandb login $WANDB_API_KEY
 sleep 1
-$CONDA_PY_PATH -m wandb login
+python -m wandb login
 echo \$?
 "
