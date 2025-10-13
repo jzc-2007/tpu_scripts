b9ac98a6c892aefe178f57353279edf55921ba0c
qiaosungithub <sqa23@mails.tsinghua.edu.cn>
2025-10-13 17:20:34 +0000

    fix bug

--- debug.sh
+++ debug.sh
@@ -5,7 +5,7 @@ mkdir tmp
 echo "start running main"
 
 # JAX_PLATFORMS=cpu python3 main.py \
-python3 main.py \
+/kmh-nfs-ssd-us-mount/code/eva/miniforge3/bin/python3 main.py \
     --workdir=$(pwd)/tmp \
     --mode=local_debug \
     --config=configs/load_config.py:local_debug \
